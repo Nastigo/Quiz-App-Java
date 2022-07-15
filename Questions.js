4fcff240c8bebee54422beb884677a64bae93912
Nastigo <100048443+Nastigo@users.noreply.github.com>
2022-07-15 09:27:02 +0200

    Add files via upload

---
+++ Questions.js
@@ -0,0 +1,92 @@
+const questions =[
+    {
+        number:1,
+        question:'Java ist eine _________ Sprache',
+        answer: 'stark typisiert',
+        options:[
+            'dynamisch typisierte',
+            'schwach typisiert',
+            'stark typisiert',
+            'mittel typisiert',
+            /*Java ist eine stark typisierte Programmiersprache, da jede Variable mit einem Datentyp deklariert werden muss. Eine Variable kann nicht gestartet werden, ohne den Wertebereich zu kennen, den sie enthalten kann, und sobald sie deklariert ist, kann sich der Datentyp der Variablen nicht ändern.*/
+        ]
+    },
+
+    {
+        number:2,
+        question:'Wie viele primitive Datentypen gibt es in der Java-Programmierung?',
+        answer: '8',
+        options:[
+            '2',
+            '3',
+            '5',
+            '8',
+            /*Primitive Typen sind die grundlegendsten Datentypen, auf die in Java zugegriffen werden kann. Es gibt 8 primitive Datentypen: int, float, boolean, byte, char, short, long und double.*/
+        ]
+    },
+
+    {
+        number:3,
+        question:'Größe von int in Java ist ______',
+        answer: '32-Bit',
+        options:[
+            '16-Bit',
+            '32-Bit',
+            '64-Bit',
+            'Hängt von der Ausführungsumgebung ab',
+            /*Die Größe von int in Java beträgt 4 Bytes = 2 ^ 4 = 32 Bit.*/
+        ]
+    },
+
+    {
+        number:4,
+        question:'Welches ist der kleinste ganzzahlige Datentyp?',
+        answer: 'Byte',
+        options:[
+            'int',
+            'Byte',
+            'kurz',
+            'lang',
+            /*Der kleinste ganzzahlige Datentyp ist Byte*/
+        ]
+    },
+
+    {
+        number:5,
+        question:'Der kleinere ganzzahlige Typ ist _______ und seine Größe beträgt __ Bits',
+        answer: 'Byte, 8',
+        options:[
+            'Byte, 8',
+            'Byte, 1',
+            'kurz, 8',
+            'int, 4',
+            /*Der kleinere ganzzahlige Typ ist Byte und hat eine Größe von 8 Bit (1 Byte = 8 Bit)*/
+        ]
+    },
+
+   {
+        number:6,
+        question:'Fließkommazahlen umfassen..',
+        answer: 'Float und Double',
+        options:[
+            'int',
+            'Float und Double',
+            'String',
+            'byte',
+        ]
+    },
+
+    {
+        number:7,
+        question:'Wie groß ist die Ganzzahl in Java?',
+        answer: '4 Bytes',
+        options:[
+            '1 Bytes',
+            '2 Bytes',
+            '4 Bytes',
+            '8 Bytes',
+            /*Primitive Typen sind die grundlegendsten Datentypen, auf die in Java zugegriffen werden kann. Es gibt 8 primitive Datentypen: int, float, boolean, byte, char, short, long und double.*/
+        ]
+    },
+
+]
